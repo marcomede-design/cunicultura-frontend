9247db80a164e1b5b084355cc5498f2e3c7a9086
Mario Luis Moreira <marcomede@gmail.com>
2026-04-22 16:26:40 -0300

    corrigir import React no Relatorios

--- src/pages/Relatorios.jsx
+++ src/pages/Relatorios.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import api from "../services/api"
 import { cores, estilosBase } from "../styles/tema"
